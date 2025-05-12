71a33d6307d533d3f54429f8e60fd73f0c3cac7d
Sevvelkaran <2k21it50@kiot.ac.in>
2025-05-12 15:31:13 +0530

    commit

--- PytestSelenium/basic_test.py
+++ PytestSelenium/basic_test.py
@@ -1,17 +1,39 @@
 import pytest
 
-def test_sample_one():
-    print('Hai')
+# def test_d():
+#     print('Hai')
 
-def test_sample_two():
+# def test_c():
+#     a =10
+#     b = 10
+#     assert a == b
+# def test_b():
+#     a = 10
+#     b = 20
+#     assert a < b
+# def test_a():
+#     a = "arun"
+#     b = "arun"
+#     assert a.__eq__(b)
+
+    #---------------------------pytest markers-----------------------#
+@pytest.mark.smoke   
+def test_d():
+    print('Hai')
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.xfail(reason = "i wanted to check that")
+def test_c():
     a =10
     b = 10
-    assert a == b
-def test_sample_three():
+    assert a != b
+@pytest.mark.skip(reason="This is an trial")
+def test_b():
     a = 10
     b = 20
     assert a < b
-def test_sample_four():
+@pytest.mark.xfail(reason = "i wanted to check that")
+def test_a():
     a = "arun"
-    b = "aruns"
+    b = "arun"
     assert a.__eq__(b)
